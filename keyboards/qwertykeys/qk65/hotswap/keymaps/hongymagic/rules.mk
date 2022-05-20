9e7d95807f716a84607958e043c7143c0a05b0ae
hongymagic <hongymagic@users.noreply.github.com>
2022-05-20 15:28:54 +1000

    Create keymaps for QwertyKeys QK65

---
+++ keyboards/qwertykeys/qk65/hotswap/keymaps/hongymagic/rules.mk
@@ -0,0 +1,7 @@
+# Build Options
+#   change yes to no to disable
+#
+MOUSEKEY_ENABLE = no        # Mouse keys
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+VIA_ENABLE = yes			# Enable VIA support
+LTO_ENABLE = yes			# Enable LTO suppor
